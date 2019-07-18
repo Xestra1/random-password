c96a4481e1ab0e25981ee365cd18f2864037dfc9
Nick <nickxz149@gmail.com>
2019-07-18 11:15:34 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,30 @@
-var symbols = ['!', '#', "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-var useSymbols = false;
+const symbols = ['!', '#', "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+let useSymbols = false;
 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var useNumbers = false;
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let useNumbers = false;
 
-var lowercase = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
-var useLowercase = false;
+const lowercase = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+let useLowercase = false;
 
-var uppercase = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
-var useUppercase = false;
+const uppercase = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+let useUppercase = false;
 
-var passwordChars = [];
+let passwordChars = [];
 
-var charLength = document.getElementById("length").value;
+let charLength = document.getElementById("length").value;
 
-document.getElementById("generate").addEventListener("click", generate, false);
 
-function generate() {
-    var charLength = document.getElementById("length").value;
-
+const generate = () => {
+    let charLength = document.getElementById("length").value;
     checkArray();
-
     appendArrays();
-
     password(passwordChars, charLength);
-
     reset();
 }
+document.getElementById("generate").addEventListener("click", generate, false);
 
-function checkArray() {
+const checkArray = () => {
     if (document.getElementById("symbols").checked === true && useSymbols === false) {
         useSymbols = true;
     } else {
@@ -54,45 +50,37 @@ function checkArray() {
     }
 }
 
-function appendArrays() {
+const appendArrays = () => {
     if (useSymbols === true) {
         passwordChars = passwordChars.concat(symbols);
-    } else {
-        console.log("Symbols not used");
     }
 
     if (useNumbers === true) {
         passwordChars = passwordChars.concat(numbers);
-    } else {
-        console.log("Symbols not used");
     }
 
     if (useLowercase === true) {
         passwordChars = passwordChars.concat(lowercase);
-    } else {
-        console.log("Symbols not used");
     }
 
     if (useUppercase === true) {
         passwordChars = passwordChars.concat(uppercase);
-    } else {
-        console.log("Symbols not used");
     }
 }
 
-function password(array, totalLength) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
+const password = (array, totalLength) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
     array.length = totalLength;
-    var password = array.join('');
+    let password = array.join('');
     document.getElementById("password").innerHTML = password;
 }
 
-function reset() {
+const reset = () => {
     useSymbols = false;
     useNumbers = false;
     useLowercase = false;
